2b6a82b00de17e8b2046e9b541c4688988999c82
Valentin <valentin.nedelcu@apprenant.ifapme.be>
2026-01-09 19:20:31 +0100

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,168 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Post
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+from .forms import EmailPostForm, CommentForm, SearchForm
+from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, TrigramSimilarity
+
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    #Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page',1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(
+        request,
+        'blog/post/list.html',
+        {
+            'posts':posts,
+            'tag':tag,
+        }
+    )
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,)
+    #List of active comments for this post
+    comments = post.comments.filter(active=True)
+    #Form for users to comment
+    form = CommentForm()
+
+    #List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.objects.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')
+
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post':post,
+         'comments':comments,
+         'form':form,
+         'similar_posts':similar_posts,
+         },
+    )
+
+def post_share(request,post_id):
+    #Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    sent= False
+
+    if request.method == 'POST':
+        #Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+                #Form fields passed validation
+            cd= form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']})"
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments : {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']],
+            )
+            sent=True
+                #... send email
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post':post,
+            'form':form,
+            'sent':sent
+        }
+    )
+@require_POST
+def post_comment(request,post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+    comment = None
+    #A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        #Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        #Assign the post to the comment
+        comment.post= post
+        #Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post':post,
+            'form':form,
+            'comment':comment,
+        }
+    )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = None
+
+    if 'query' in request.GET:
+        form= SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+
+            search_vector = SearchVector('title','body')
+            search_query = SearchQuery(query )
+
+            results = (
+                Post.published.annotate(
+                    search= search_vector,
+                    rank= SearchRank(search_vector, search_query),
+                   # similarity= TrigramSimilarity('title',query),
+                )
+                .filter(search=search_query)   #search=search_query in parameter or similarity__gt=0.1 for trigram (to be inspected)
+                .order_by('-rank') #-rank in parameter or -similarity for trigram (to be inspected)
+            )
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form':form,
+            'query':query,
+            'results':results,
+        }
+    )
